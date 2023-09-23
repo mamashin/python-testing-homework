f96ab850301932dcfed84aeb4d62e0cbc0eee0e4
Nikolay Mamashin <mamashin@gmail.com>
2023-09-23 12:02:11 +0200

    Revert "Now can delete item from favorite list"
    This reverts commit 00370d22036e07690489c8a573d00980af5cbf20.

--- server/apps/pictures/views.py
+++ server/apps/pictures/views.py
@@ -5,10 +5,10 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import QuerySet
 from django.http import HttpResponse
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView, FormView
+from django.views.generic import CreateView, ListView, TemplateView
 
 from server.apps.pictures.container import container
-from server.apps.pictures.intrastructure.django.forms import FavouritesForm, DeleteFavouritesForm
+from server.apps.pictures.intrastructure.django.forms import FavouritesForm
 from server.apps.pictures.logic.usecases import favourites_list, pictures_fetch
 from server.apps.pictures.models import FavouritePicture
 from server.common.django.decorators import dispatch_decorator
@@ -61,35 +61,12 @@ class DashboardView(CreateView[FavouritePicture, FavouritesForm]):
 
 @final
 @dispatch_decorator(login_required)
-class FavouritePicturesView(FormView[FavouritePicture, ]):
-    """Show and delete the :term:`favourites`."""
+class FavouritePicturesView(ListView[FavouritePicture]):
+    """View the :term:`favourites`."""
 
-    form_class = DeleteFavouritesForm
     template_name = 'pictures/pages/favourites.html'
-    success_url = reverse_lazy('pictures:favourites')
 
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        """Inject extra context to template rendering."""
+    def get_queryset(self) -> QuerySet[FavouritePicture]:
+        """Return matching pictures."""
         list_favourites = container.instantiate(favourites_list.FavouritesList)
-
-        context = super().get_context_data(**kwargs)
-        context['pictures'] = list_favourites(self.request.user.id)  # sync http call, may hang
-        return context
-
-    def get_form_kwargs(self) -> dict[str, Any]:
-        """Add current user to the context."""
-        base_kwargs = super().get_form_kwargs()
-        base_kwargs['user'] = self.request.user
-        return base_kwargs
-
-    def form_valid(self, form: DeleteFavouritesForm) -> HttpResponse:
-        """Simple check if data is valid."""
-        try:
-            form.delete()
-            messages.warning(self.request, 'Удалено')
-            return super().form_valid(form)
-        except KeyError:
-            messages.error(self.request, 'Нет такой записи !')
-            response = super().form_invalid(form)
-            response.status_code = 400
-            return response
+        return list_favourites(self.request.user.id)
